0d158a9077797db0b46478db7d41c59d2a8e68ed
hve <hve4638@gmail.com>
2026-04-03 16:12:37 +0900

    chore: bump version to 0.4.0

--- src/fsonl/__init__.py
+++ src/fsonl/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 from ._errors import FsonlError, ParseError, SchemaError, BindError
 from ._types import ParseResult, RawEntry, SchemaDirective, SchemaParam, ParamKind, ExtraFieldPolicy, OMIT
